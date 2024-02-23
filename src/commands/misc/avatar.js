8485bef27bb76b3772acdd312484d2a56887e6f0
Jake Mackie <jake.wmackie@gmail.com>
2024-02-23 01:15:25 +0000

    Global avatar command

---
+++ src/commands/misc/avatar.js
@@ -0,0 +1,45 @@
+const {
+    ApplicationCommandOptionType,
+    EmbedBuilder,
+    ButtonBuilder,
+    ActionRowBuilder,
+    ButtonStyle,
+} = require('discord.js');
+
+module.exports = {
+    callback: (client, interaction) => {
+        let user = interaction.options.getUser('user') || interaction.member;
+        if (!user)
+            return interaction.reply({
+                content: `Ack! I couldn't find that user, please try again.`,
+                ephemeral: true,
+            });
+
+        let userAvatar = user.displayAvatarURL({ dynamic: true, size: 512 });
+
+        const embed = new EmbedBuilder()
+            .setDescription(`${user}'s avatar`)
+            .setImage(userAvatar)
+            .setTimestamp();
+
+        const button = new ButtonBuilder()
+            .setLabel('Download')
+            .setStyle(ButtonStyle.Link)
+            .setURL(`${user.displayAvatarURL({ dynamic: true, size: 512 })}`);
+
+        const row = new ActionRowBuilder().addComponents(button);
+
+        interaction.reply({ embeds: [embed], components: [row] });
+    },
+
+    name: 'avatar',
+    description: 'Returns the avatar of the user',
+    options: [
+        {
+            name: 'user',
+            description: 'The user to fetch',
+            type: ApplicationCommandOptionType.User,
+            required: false,
+        },
+    ],
+};
